69bbd7bb6bef53b7bc0deac79a59e6aa7931e615
iamhaao <haolee2002tst@gmail.com>
2024-05-06 23:20:34 +0700

    set up FE

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#33424f",
+        subMain: "#ff6600",
+      },
+      fontFamily: {
+        euclid: ["Euclid Circular A", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
