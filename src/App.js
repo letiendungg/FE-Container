b8130e43b66f85dcbbe4d7de0cf7aeb83ea928d2
lenguyentrinh <lethinguyentrinh.work@gmail.com>
2024-05-09 16:27:57 +0700

    fix/signup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Page/Home";
 import Login from "./Page/Login";
+import Signup from "./Page/Signup";
 function App() {
   return (
     <>
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<Signup />} />
         </Routes>
       </Router>
     </>
---
+++ src/Data/OptionNation.js
@@ -0,0 +1,19 @@
+const OptionNation = [
+  {
+    title: "Viet Nam",
+  },
+  {
+    title: "United States",
+  },
+  {
+    title: "United Kingdom",
+  },
+  {
+    title: "Germany",
+  },
+  {
+    title: "France",
+  },
+];
+
+module.exports = OptionNation;
